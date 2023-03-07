a633d64a9b20d4165f26c8346c6362ef20dd2c4f
cb1258 <cb1258>
2023-03-07 10:39:01 +0000

    modified:   logging/log.txt 	modified:   repoUpdate/fetchResult.txt 	modified:   repoUpdate/repoUpdater.sh

--- repoUpdate/repoUpdater.sh
+++ repoUpdate/repoUpdater.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 #! /bin/sh
+#! /bin/bash -e 
 
 # Author: Group Alt 255 | <jm1460@canterbury.ac.uk><cb1258@canterbury.ac.uk><CJ><or56@canterbury.ac.uk>
 # Version 1.0
@@ -13,8 +14,20 @@ cd /home/pi/github/smartsystemsmonitor-alt-255
 
 repository=$(pwd)
 
-
 LOGGING "- Current Directory is: ${repository}"
-#LOGGING "- Repository Update Script was run"
 
-#if 
+if [ -f /home/pi/github/smartsystemsmonitor-alt-255/repoUpdate/fetchResult.txt ]; then
+	if [ ! -s /home/pi/github/smartsystemsmonitor-alt-255/repoUpdate/fetchResult.txt ]; then
+		#Fetch result is empty
+		LOGGING "- fetchResult.txt was empty - Running Fetch now."
+		echo -e "this file now contains more information" >> /home/pi/github/smartsystemsmonitor-alt-255/repoUpdate/fetchResult.txt
+		git fetch >> /home/pi/github/smartsystemsmonitor-alt-255/repoUpdate/fetchResult.txt
+	else
+		LOGGING "
+
+
+Other condition met, Will not run a fetch"
+	fi
+else
+	LOGGING "fetchResult.txt does not exist."
+fi
