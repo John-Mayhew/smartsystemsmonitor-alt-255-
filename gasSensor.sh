66619b79feca1d76cc1055a331af9d4d4b9e455a
oliverrushgadsby <78021990+oliverrushgadsby@users.noreply.github.com>
2023-03-09 12:02:25 +0000

    Added info to gasSensor.sh

--- gasSensor.sh
+++ gasSensor.sh
@@ -1,1 +1,23 @@
-
+#! /usr/bin/env bash
+#! /bin/sh
+#! /bin/bash -e
+#
+# Author: Group Alt 255
+#
+# Contributors:
+#
+# John Mayhew           |       <jm1460@canterbury.ac.uk>
+# Cameron Browne        |       <cb1258@canterbury.ac.uk>
+# Cj Wilson             |       <c.wilson831@canterbury.ac.uk>
+# Oliver Rush-Gadsby    |       <or56@canterbury.ac.uk>
+#
+# Version 1.0
+# Date Created: 09/03/2023
+# Date of Version Completion:
+#
+# Description:
+# This script is created in order to obtain data from the gas sensor on the Raspberry Pi where this data will eventually be sent to an IP Address.
+#
+#
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------- Declaring Variables
