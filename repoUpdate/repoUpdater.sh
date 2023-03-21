df7d658e593570f15a0c292958ca9ed1b1562f3b
John-Mayhew <JM1460@canterbury.ac.uk>
2023-03-21 10:17:41 +0000

    Update repoUpdater.sh
    Attempt to force git to re-run the pull until successful based on error codes
    
    1=fail
    0=success
    
    Comments added to the code.

--- repoUpdate/repoUpdater.sh
+++ repoUpdate/repoUpdater.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
-#! /bin/sh
-#! /bin/bash -e
+
 #
 # Author: Group Alt 255
 #
@@ -23,23 +22,38 @@
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # --------------------------------------- Declaring Variables
 
-source /home/pi/github/smartsystemsmonitor-alt-255/logging/./logging.sh # Pulls the funciton script for logging
+source /home/pi/github/smartsystemsmonitor-alt-255/logging/./logging.sh # Sources the logging functin script.
 
-cd /home/pi/github/smartsystemsmonitor-alt-255 # Sets the directory that the script will run from, allows for access to git commands becuase they need visability of the .git files
+cd /home/pi/github/smartsystemsmonitor-alt-255 # Sets the directory that the script will run from, allows for access to git commands because they require visability of the .git files.
 
 repository=$(pwd) # Prints the working directory and parses this value into $Repository.
-status=$(git fetch -va 2>&1 | grep -w main | grep -w "[up to date]")
+status=$(git fetch -va 2>&1 | grep -w main | grep -w "[up to date]") # Defines the variable $status and parses the result of the git fetch to whether main is up to date or requires as update.
 
 LOGGING "- Current Directory is: ${repository}" # Prints the current working directory in the log.txt file.
 
-if [[ ! ${status} ]]; then
+if [[ ! ${status} ]]; then # If Git fetch returns anything other then "up-to-date" then the git fetch, the git pull will be run.
 
-	LOGGING "- Repository requires an update, updating now"
-	gitPull=$(git pull 2>&1)
-	LOGGING "${gitPull} | grep -w main"
+	LOGGING "- Repository requires an update, updating now" # Logging to say that the local repository does not match the remote repository and therefore requires an update. 
+	gitPull=$(git pull -va 2>&1) # Parses the result of git pull to a variable so that this can be used later for logging.
+	LOGGING "${gitPull} | grep -w main" # Logging the git pull so that we can monitor failures.
+	LOGGING "- Status of git Pull: $?" # Logs the exit code of git pull for monitoring purposes, used to initiate a re-run if failure occurs.
+	
+	while [ $? != 0 ]; # While exit code is not 0 (successful) this will re-run the git pull incase of failure.
+	do
+		
+		git pull # Pulls from the remote repository
+		LOGGING "- Git pull failed (exit code 1), running git pull again" # Logging the failure every time the loop is run - Should run until completed. 
+		z
+		if [ $? == 0 ]; then # If exit code = 0 (successful) then the git pull has completed without failure.
+		
+			LOGGING "- Git Pull successful, Exit code 0, process sleeping until next scheduled event" # Logging that the git pull was successful. 
+		
+		fi
+		
+	done
 
 else
 
-	LOGGING "- Repository up to date"
-  
+	LOGGING "- Repository up to date" # Logging that the repository has not changed and therefore does not require an update. 
+	
 fi
