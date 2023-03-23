f43cf37d5fc0cba7437be04c2767af6aa32745ad
CameronBrowne1258 <78022298+CameronBrowne1258@users.noreply.github.com>
2023-03-23 12:15:41 +0000

    Update setSpeed.sh
    added a header to the script

--- CPU/setSpeed.sh
+++ CPU/setSpeed.sh
@@ -1,11 +1,34 @@
-# change the operating freq of nodes update min and max to be the same to ensure true speed. 
+#! /usr/bin/env bash
+#! /bin/sh
+#! /bin/bash -e
+#
+# Author: Group Alt 255
+#
+# Contributors:
+#
+# John Mayhew           |       <jm1460@canterbury.ac.uk>
+# Cameron Browne        |       <cb1258@canterbury.ac.uk>
+# Cj Wilson             |       <c.wilson831@canterbury.ac.uk>
+# Oliver Rush-Gadsby     |       <or56@canterbury.ac.uk>
+#
+# Version 1.0
+# Date Created: 23/03/2023
+# Date of Version Completion:
+#
+# Description:
+# This script is created to change frequencies and then the speed of the CPU.
+# This script works along side cpuInfo.sh and setGov.sh, which are used to change the governor and CPU speeds.
+#
+#
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------- Declaring Variables 
 FREQ=$1
 CPU="/sys/devices/system/cpu/"
 MAX="scaling_max_freq"
 MIN="scaling_min_freq"
-
+# this is used to make the min and max the same as one another so that scaling is the same.
 CORES=$(lscpu | grep -w "CPU(s):*" | awk 'NR==1 {print$2}')
-# make sure in sudo mode
+# sudo allows
 sudo echo
 for (( c=0; c<${CORES}; c++))
 do
