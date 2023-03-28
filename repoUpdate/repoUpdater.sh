72f6016abf17b596797d7582c2b52124c66ae491
cb1258 <cb1258>
2023-03-28 13:40:04 +0100

    modified:   repoUpdater.sh

--- repoUpdate/repoUpdater.sh
+++ repoUpdate/repoUpdater.sh
@@ -8,7 +8,7 @@
 # John Mayhew           |       <jm1460@canterbury.ac.uk>
 # Cameron Browne        |       <cb1258@canterbury.ac.uk>
 # Cj Wilson             |       <cw831@canterbury.ac.uk>
-# Oliver Rush-Gadsby     |       <or56@canterbury.ac.uk>
+# Oliver Rush-Gadsby    |       <or56@canterbury.ac.uk>
 #
 # Version 1.0
 # Date Created: 01/03/2023
@@ -31,8 +31,6 @@ status=$(git fetch -va 2>&1 | grep -w main | grep -w "[up to date]") # Defines t
 fetchCount=0 # Break condition for while loop to prevent indefinite retries of git fetch
 pullCount=0 # Break conditon for the while loop performing the git pull incase of failure to prevent indefinite retries. 
 
-#LOGGING "- Current Directory is: ${repository}" # Prints the current working directory in the log.txt file.
-
 while [[ -z ${status} && ${fetchCount} -lt 10 ]]; do
 
 	#LOGGING "- Status is empty, Git fetch may have failed - Retrying now" # Logging for if the status variable is empty
@@ -46,25 +44,24 @@ if [[ ! ${status} ]]; then # If Git fetch returns anything other then "up-to-dat
 
 	LOGGING "- Repository requires an update, updating now" # Logging to say that the local repository does not match the remote repository and therefore requires an update. 
 	gitPull=$(git pull -va 2>&1 | grep -w "main") # Parses the result of git pull to a variable so that this can be used later for logging.
-	LOGGING "$({gitPull}" # Logging the git pull so that we can monitor failures.
+	LOGGING "$({gitPull})" # Logging the git pull so that we can monitor failures.
 	LOGGING "- Status of git Pull: $?" # Logs the exit code of git pull for monitoring purposes, used to initiate a re-run if failure occurs.
-	
+
 	while [[ $? != 0 && ${pullCount} -lt 10 ]]; # While exit code is not 0 (successful) this will re-run the git pull incase of failure.
 	do
-		
+
 		git pull # Pulls from the remote repository
 		LOGGING "- Git pull failed (exit code 1), running git pull again" # Logging the failure every time the loop is run - Should run until completed. 
 		sleep 60
 		let pullCount++ # Increases the variable by 1 so that the while loop will break after 10 tries if not successful and the error code is still reporting as 1 (Failure)
-#		echo "${pullCount}"
-		
-		if [ $? == 0 ]; then # If exit code = 0 (successful) then the git pull has completed without failure.
-		
+
+		if [[ $? == 0 ]]; then # If exit code = 0 (successful) then the git pull has completed without failure.
+
 			LOGGING "- Git Pull successful, Exit code 0, process sleeping until next scheduled event" # Logging that the git pull was successful. 
 			echo $?
-		
+
 		fi
-		
+
 	done
 
 elif [[ ${status} ]]; then
@@ -74,6 +71,6 @@ elif [[ ${status} ]]; then
 
 else
 
-	LOGGING "Else was met - ${status}"
+	LOGGING "- Else condition was met - $({status})"
 
 fi
