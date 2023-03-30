844e9ce97ec2069b851f4454de7f88d2e1a74d50
CameronBrowne1258 <78022298+CameronBrowne1258@users.noreply.github.com>
2023-03-30 12:36:58 +0100

    Update tempHumidSensor.py
    extra info

--- tempHumidSensors/tempHumidSensor.py
+++ tempHumidSensors/tempHumidSensor.py
@@ -1,4 +1,24 @@
-import os
+#! /usr/bin/env bash
+#! /bin/sh
+#! /bin/bash -e
+#
+# Author: Group Alt 255
+#
+# Contributors:
+#
+# John Mayhew           |       <jm1460@canterbury.ac.uk>
+# Cameron Browne        |       <cb1258@canterbury.ac.uk>
+# Cj Wilson             |       <c.wilson831@canterbury.ac.uk>
+# Oliver Rushgadsby     |       <or56@canterbury.ac.uk>
+#
+# Version 1.0
+# Date Created: 30/03/2023
+# Date of Version Completion: 30/03/2023
+#
+# This script was created to take temperature and sensor readings
+#
+#
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------import os
 import time
 from datetime import datetime
 import csv
@@ -15,10 +35,10 @@ def main():
 	with open(file_path, 'w', newline='') as file:
 		writer = csv.writer(file)
 		writer.writerow(["Timestamp", "Temperature", "Humidity"])
-
+# this allows for it to write the readings to readings.csv
 		while True:
 			humi, temp = sensor.read()
-			if not humi is None:
+			if not humi is None: # if there are readings that can be found, print these with a timestamp.
 				timestamp = datetime.utcnow().isoformat()
 				print('humidity {1:.1f}%, temperature {2:.1f}*'.format(sensor.dht_type, humi, temp))
 				writer.writerow([timestamp, humi, temp])
