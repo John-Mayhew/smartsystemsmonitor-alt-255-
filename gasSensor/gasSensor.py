687acda98ee7d5b6ff05943401fea6d5144ba01e
oliverrushgadsby <78021990+oliverrushgadsby@users.noreply.github.com>
2023-03-30 09:30:18 +0100

    Update gasSensor.py

--- gasSensor/gasSensor.py
+++ gasSensor/gasSensor.py
@@ -18,53 +18,25 @@
 #
 #----------------------------------------------------------------------------------------------------
 
-import csv
-import os
 import sys
 import time
 from datetime import datetime
 
-#import grovepi
-#from grove.adc import ADC
-#from envbash import load_envbash
-
 sys.path.insert(0, '/home/pi/grove.py/grove')
 from grove_gas_sensor_mq5 import GroveGasSensorMQ5
 
-# connect sensor to analog port
-PIN = "0"
-sensor = GroveGasSensorMQ5(PIN)
+sensor = GroveGasSensorMQ5(0) # connect sensor to analog port A0
 
-# logging (?)
-#load_envbash('/home/pi/github/smartsystemsmonitor-alt-255/logging/./logging.sh')
-#os.environ['LOGGING "- Creating new gas readings"']
-
-directory = '/home/pi/github/smartsystemsmonitor-alt-255/logging'
-filename = "gas_sensor_data.csv"
-
-file_path = os.path.join(directory, filename)
-
-# open file to store data
-with open(file_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    # add headings to file
-    writer.writerow(["Timestamp", "Gas Sensor Data (ppm)"])
-    
-    while True:
-          try:
-            # get and print the reading 
-            timestamp = datetime.utcnow().isoformat()
-            gas_reading = sensor.MQ5
-
-            print('{0} - Gas value: {1}'.format(timestamp, gas_reading))
+while True:
+      try:
+        # get the time stamp and reading 
+        timestamp = datetime.utcnow().isoformat()
+        gas_reading = sensor.MQ5
         
-            # write data to file
-            writer.writerow([timestamp, gas_reading])
+        print('{0} - Gas value: {1}'.format(timestamp, gas_reading)) # print the values
         
-            # sleep before getting a new reading
-            time.sleep(1)
-            # if ctrl+c is pressed; stop
-          except KeyboardInterrupt:
-            break
-
+        time.sleep(1) # sleep before getting a new reading
+        
+      except KeyboardInterrupt:  # if ctrl+c is pressed; stop
+        break
 # print(dir(sensor))
