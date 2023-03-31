0c614359d310d97e288cba5bfa5398ab5037ef05
CameronBrowne1258 <78022298+CameronBrowne1258@users.noreply.github.com>
2023-03-31 11:21:28 +0100

    Update gasSensor.py
    removed conflict errors in code

--- gasSensor/gasSensor.py
+++ gasSensor/gasSensor.py
@@ -36,27 +36,17 @@ file_path = os.path.join(directory, filename)
 
 def main():
     sensor = GroveGasSensorMQ5(0) # connect sensor to analog port A0
-<<<<<<< HEAD
-
-=======
     
     # use the logging script to make a log that this script ran 
     #load_envbash('/home/pi/github/smartsystemsmonitor-alt-255/logging/./logging.sh')
     #os.environ['LOGGING "- Creating new gas readings"']
-    
->>>>>>> 1236eaacbf970f6f7710049e86046254d80b49df
+
     with open(file_path, 'w', newline='') as file: # open file
         writer = csv.writer(file)
         writer.writerow(["Timestamp", "Gas Value"]) # write headings to file
 
-<<<<<<< HEAD
-        for i in range(10):
-
-            # get the time stamp and gas reading
-=======
         for i in range(10): 
             # get the time stamp and gas reading 
->>>>>>> 1236eaacbf970f6f7710049e86046254d80b49df
              timestamp = datetime.utcnow().isoformat()
              gas_reading = sensor.MQ5
 
@@ -64,9 +54,4 @@ def main():
 
              writer.writerow([timestamp, gas_reading]) # write timestamp and gas values to file
              time.sleep(1) # sleep before getting a new reading
-<<<<<<< HEAD
-
-=======
-           
->>>>>>> 1236eaacbf970f6f7710049e86046254d80b49df
 main()
